36204d7e9d55718561ba1b1daa58cecb40012635
sagar <sag9315@gmail.com>
2021-09-25 06:17:03 +0530

    comment improved for subsets - function array problem

--- DSA Level 1 (Foundation)/Function and Arrays/SubsetsOfArray.java
+++ DSA Level 1 (Foundation)/Function and Arrays/SubsetsOfArray.java
@@ -11,7 +11,7 @@ public static void main(String[] args) throws Exception {
         arr[i] = scn.nextInt();
     
     int lim = 1<<n; // total combinations for power-set of n-distinct items => 2^n
-    for(int i=0; i<lim; i++) { // total combinations
+    for(int i=0; i<lim; i++) { // total combinations possible for subsets / subsequence(it excepts null/empty set)
         StringBuilder sb = new StringBuilder();
         int decimal = i;
         
@@ -19,10 +19,12 @@ public static void main(String[] args) throws Exception {
             int r = decimal % 2;
             decimal /= 2;
             
+            // treating 1 binary-> item present
+            // and 0 for absent
             if(r==0)
                 sb.insert(0, "-\t");
             else
-                sb.insert(0, arr[n-1-j] + "\t"); // last item is accessed in first
+                sb.insert(0, arr[n-1-j] + "\t"); // decimal->binary , last remainder is the first digit in binary system 
         }
         System.out.println(sb);
     }
